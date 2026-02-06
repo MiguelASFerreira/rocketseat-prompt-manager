59a74dc4fa988eaa00a67197c72bcbb61251bd5b
Miguel <miguel.santos@grupointegrado.br>
2026-02-06 11:38:30 -0300

    feat: Enhancing the Creation Form

--- src/components/prompts/prompt-form.tsx
+++ src/components/prompts/prompt-form.tsx
@@ -1,26 +1,68 @@
-import { Button } from "../ui/button"
-import { Input } from "../ui/input"
-import { Textarea } from "../ui/textarea"
+'use client';
+
+import { useForm } from 'react-hook-form';
+import { Button } from '../ui/button';
+import { Input } from '../ui/input';
+import { Textarea } from '../ui/textarea';
+import {
+  CreatePromptDTO,
+  createPromptSchema,
+} from '@/core/application/prompts/create-prompt.dto';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Form, FormControl, FormField, FormItem } from '../ui/form';
 
 export const PromptForm = () => {
+  const form = useForm<CreatePromptDTO>({
+    resolver: zodResolver(createPromptSchema),
+    defaultValues: {
+      title: '',
+      content: '',
+    },
+  });
   return (
-    <form action="" className="space-y-6">
-      <header className="flex flex-wrap gap-2 items-center mb-6 justify-end">
-        <Button type="submit" size={"sm"}>Salvar</Button>
-      </header>
+    <Form {...form}>
+      <form action="" className="space-y-6">
+        <header className="flex flex-wrap gap-2 items-center mb-6 justify-end">
+          <Button type="submit" size={'sm'}>
+            Salvar
+          </Button>
+        </header>
 
-      <Input 
-        placeholder="Título"
-        variant={"transparent"}
-        size={"lg"}
-        autoFocus
-      />
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  placeholder="Título"
+                  variant={'transparent'}
+                  size={'lg'}
+                  autoFocus
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
 
-      <Textarea
-        placeholder="Digite o conteúdo do prompt..."
-        variant="transparent"
-        size={"lg"}
-      />
-    </form>
-  )
-}
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Textarea
+                  placeholder="Digite o conteúdo do prompt..."
+                  variant="transparent"
+                  size={'lg'}
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+};
