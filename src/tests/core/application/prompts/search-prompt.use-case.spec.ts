eccaaf22600a07fa1c7ec830911bb0ad0848b091
MiguelASFerreira <migant1306@gmail.com>
2026-02-17 16:54:13 -0300

    feat: Add findByTitle method to prompt repository

--- src/tests/core/application/prompts/search-prompt.use-case.spec.ts
+++ src/tests/core/application/prompts/search-prompt.use-case.spec.ts
@@ -1,20 +1,36 @@
-import { SearchPromptsUseCase } from "@/core/application/prompts/search-prompts.use-case";
-import type { Prompt } from "@/core/domain/prompts/prompt.entity";
-import type { PromptRepository } from "@/core/domain/prompts/prompt.repository";
+import { SearchPromptsUseCase } from '@/core/application/prompts/search-prompts.use-case';
+import type { Prompt } from '@/core/domain/prompts/prompt.entity';
+import type { PromptRepository } from '@/core/domain/prompts/prompt.repository';
 
 describe('SearchPromptsUseCase', () => {
   const input: Prompt[] = [
-      { id: '1', title: 'AI Prompt 1', content: 'Content 1', createdAt: new Date(), updatedAt: new Date() },
-      { id: '2', title: 'AI Prompt 2', content: 'Content 2', createdAt: new Date(), updatedAt: new Date() },
-    ];
-    
+    {
+      id: '1',
+      title: 'AI Prompt 1',
+      content: 'Content 1',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      id: '2',
+      title: 'AI Prompt 2',
+      content: 'Content 2',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  ];
+
   const repository: PromptRepository = {
+    create: async () => Promise.resolve(),
     findMany: async () => input,
-    searchMany: async (term) => input.filter(
-      (prompt) => 
-        prompt.title.toLowerCase().includes(term.toLocaleLowerCase()) || 
-        prompt.content.toLowerCase().includes(term.toLocaleLowerCase())),
-  }
+    findByTitle: async () => null,
+    searchMany: async (term) =>
+      input.filter(
+        (prompt) =>
+          prompt.title.toLowerCase().includes(term.toLocaleLowerCase()) ||
+          prompt.content.toLowerCase().includes(term.toLocaleLowerCase())
+      ),
+  };
 
   it('deve retornar todos os prompts quando o termo de busca estiver vazio', async () => {
     const useCase = new SearchPromptsUseCase(repository);
@@ -32,7 +48,7 @@ describe('SearchPromptsUseCase', () => {
 
     expect(results).toHaveLength(1);
     expect(results[0].id).toBe('2');
-  })
+  });
 
   it('deve aplicar trim em buscas com termo com espaços em branco e retornar toda a lista de prompts', async () => {
     const findMany = jest.fn().mockResolvedValueOnce(input);
@@ -41,7 +57,7 @@ describe('SearchPromptsUseCase', () => {
       ...repository,
       findMany,
       searchMany,
-    }
+    };
 
     const useCase = new SearchPromptsUseCase(repositoryWithSpies);
     const query = '    ';
@@ -49,9 +65,9 @@ describe('SearchPromptsUseCase', () => {
     const results = await useCase.execute(query);
 
     expect(results).toHaveLength(2);
-    expect(findMany).toHaveBeenCalledTimes(1)
-    expect(searchMany).not.toHaveBeenCalled()
-  })
+    expect(findMany).toHaveBeenCalledTimes(1);
+    expect(searchMany).not.toHaveBeenCalled();
+  });
 
   it('deve buscar termo com espaços em branco, tatando com trim', async () => {
     const firstElement = input.slice(0, 1);
@@ -62,7 +78,7 @@ describe('SearchPromptsUseCase', () => {
       ...repository,
       findMany,
       searchMany,
-    }
+    };
 
     const useCase = new SearchPromptsUseCase(repositoryWithSpies);
     const query = '  Prompt 1  ';
@@ -72,7 +88,7 @@ describe('SearchPromptsUseCase', () => {
     expect(results).toMatchObject(firstElement);
     expect(searchMany).toHaveBeenCalledWith(query.trim());
     expect(findMany).not.toHaveBeenCalled();
-  })
+  });
 
   it('deve lidar com termo undefined ou null e retornar a lista completa de prompts', async () => {
     const firstElement = input.slice(0, 1);
@@ -83,7 +99,7 @@ describe('SearchPromptsUseCase', () => {
       ...repository,
       findMany,
       searchMany,
-    }
+    };
 
     const useCase = new SearchPromptsUseCase(repositoryWithSpies);
     const query = undefined as unknown as string;
@@ -92,6 +108,6 @@ describe('SearchPromptsUseCase', () => {
 
     expect(results).toMatchObject(input);
     expect(findMany).toHaveBeenCalledTimes(1);
-    expect(searchMany).not.toHaveBeenCalled();    
-  })
-})
+    expect(searchMany).not.toHaveBeenCalled();
+  });
+});
